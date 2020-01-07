8abb711a6a66b16d15582313aac5ee349b551d89
Jingshao Chen <jingshaochen@gmail.com>
2020-01-07 10:27:28 -0600

    add tests

---
+++ test/test.js
@@ -0,0 +1,148 @@
+const { authenticate } = require('../index.js')
+
+describe('ldap-authentication test', () => {
+    test('Use an admin user to authenticate a regular user', async () => {
+      let options = {
+        ldapOpts: {
+          url: 'ldap://ldap.forumsys.com'
+        },
+        adminDn: 'cn=read-only-admin,dc=example,dc=com',
+        adminPassword: 'password',
+        userPassword: 'password',
+        userSearchBase: 'dc=example,dc=com',
+        userSearchString: '(uid=gauss)'
+      }
+      let user = await authenticate(options)
+      expect(user).toBeTruthy()
+      expect(user.uid).toEqual('gauss')
+    })
+    test('Use an regular user to authenticate iteself', async () => {
+      let options = {
+        ldapOpts: {
+          url: 'ldap://ldap.forumsys.com'
+        },
+        userDn: 'uid=einstein,dc=example,dc=com',
+        userPassword: 'password',
+        userSearchBase: 'dc=example,dc=com',
+        userSearchString: '(uid=gauss)'
+      }
+      let user = await authenticate(options)
+      expect(user).toBeTruthy()
+      expect(user.uid).toEqual('gauss')
+    })
+})
+
+describe('ldap-authentication negative test', () => {
+  test('wrong admin user should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      adminDn: 'cn=not-exist,dc=example,dc=com',
+      adminPassword: 'password',
+      userPassword: 'password',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=gauss)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+  test('wrong admin password should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      adminDn: 'cn=read-only-admin,dc=example,dc=com',
+      adminPassword: 'wrongpassword',
+      userPassword: 'password',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=gauss)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+  test('admin auth wrong username should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      adminDn: 'cn=read-only-admin,dc=example,dc=com',
+      adminPassword: 'password',
+      userPassword: 'password',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=not-exist)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+  test('admin auth wrong user password should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      adminDn: 'cn=read-only-admin,dc=example,dc=com',
+      adminPassword: 'password',
+      userPassword: 'wrongpassword',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=gauss)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+  test('user auth wrong username should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      userDn: 'cn=not-exist,dc=example,dc=com',
+      userPassword: 'password',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=gauss)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+  test('user auth wrong user password should fail', async () => {
+    let options = {
+      ldapOpts: {
+        url: 'ldap://ldap.forumsys.com'
+      },
+      userDn: 'cn=gauss,dc=example,dc=com',
+      userPassword: 'wrongpassword',
+      userSearchBase: 'dc=example,dc=com',
+      userSearchString: '(uid=gauss)'
+    }
+    let e = null
+    try {
+      await authenticate(options)
+    } catch(error) {
+      e = error
+    } 
+    expect(e).toBeTruthy()
+  })
+})
