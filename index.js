f4d953318391868f329171223567b5677cb1b9e8
Marc Welp <m.welp@phytec.de>
2021-07-20 00:13:04 +0200

    add the ldap attributes object to return object

--- index.js
+++ index.js
@@ -3,346 +3,347 @@ const ldap = require('ldapjs')
 
 // bind and return the ldap client
 function _ldapBind(dn, password, starttls, ldapOpts) {
-  return new Promise(function (resolve, reject) {
-    ldapOpts.connectTimeout = ldapOpts.connectTimeout || 5000
-    var client = ldap.createClient(ldapOpts)
+    return new Promise(function (resolve, reject) {
+        ldapOpts.connectTimeout = ldapOpts.connectTimeout || 5000
+        var client = ldap.createClient(ldapOpts)
 
-    client.on('connect', function () {
-      if (starttls) {
-        client.starttls(ldapOpts.tlsOptions, null, function (error) {
-          if (error) {
-            reject(error)
-            return
-          }
-          client.bind(dn, password, function (err) {
-            if (err) {
-              reject(err)
-              client.unbind()
-              return
+        client.on('connect', function () {
+            if (starttls) {
+                client.starttls(ldapOpts.tlsOptions, null, function (error) {
+                    if (error) {
+                        reject(error)
+                        return
+                    }
+                    client.bind(dn, password, function (err) {
+                        if (err) {
+                            reject(err)
+                            client.unbind()
+                            return
+                        }
+                        ldapOpts.log && ldapOpts.log.trace('bind success!')
+                        resolve(client)
+                    })
+                })
+            } else {
+                client.bind(dn, password, function (err) {
+                    if (err) {
+                        reject(err)
+                        client.unbind()
+                        return
+                    }
+                    ldapOpts.log && ldapOpts.log.trace('bind success!')
+                    resolve(client)
+                })
             }
-            ldapOpts.log && ldapOpts.log.trace('bind success!')
-            resolve(client)
-          })
-        })
-      } else {
-        client.bind(dn, password, function (err) {
-          if (err) {
-            reject(err)
-            client.unbind()
-            return
-          }
-          ldapOpts.log && ldapOpts.log.trace('bind success!')
-          resolve(client)
-        })
-      }
-    });
+        });
 
-    //Fix for issue https://github.com/shaozi/ldap-authentication/issues/13
-    client.on('timeout', (err) => {
-      reject(err);
-    });
-    client.on('connectTimeout', (err) => {
-      reject(err);
-    });
-    client.on('error', (err) => {
-      reject(err);
-    });
+        //Fix for issue https://github.com/shaozi/ldap-authentication/issues/13
+        client.on('timeout', (err) => {
+            reject(err);
+        });
+        client.on('connectTimeout', (err) => {
+            reject(err);
+        });
+        client.on('error', (err) => {
+            reject(err);
+        });
 
-    client.on('connectError', function(error){
-      if (error) {
-        reject(error)
-        return
-      }
-    });
+        client.on('connectError', function (error) {
+            if (error) {
+                reject(error)
+                return
+            }
+        });
 
-  });
+    });
 }
 
 // search a user and return the object
 async function _searchUser(
-  ldapClient,
-  searchBase,
-  usernameAttribute,
-  username
+    ldapClient,
+    searchBase,
+    usernameAttribute,
+    username
 ) {
-  return new Promise(function (resolve, reject) {
-    var filter = new ldap.filters.EqualityFilter({
-      attribute: usernameAttribute,
-      value: username,
+    return new Promise(function (resolve, reject) {
+        var filter = new ldap.filters.EqualityFilter({
+            attribute: usernameAttribute,
+            value: username,
+        })
+        ldapClient.search(
+            searchBase,
+            {
+                filter: filter,
+                scope: 'sub',
+            },
+            function (err, res) {
+                var ldapObject = null
+                if (err) {
+                    reject(err)
+                    ldapClient.unbind()
+                    return
+                }
+                res.on('searchEntry', function (entry) {
+                    ldapObject.user = entry.object
+                    ldapObject.attributes = entry.attributes;
+                })
+                res.on('searchReference', function (referral) {
+                    console.log('referral: ' + referral.uris.join())
+                })
+                res.on('error', function (err) {
+                    reject(err)
+                    ldapClient.unbind()
+                })
+                res.on('end', function (result) {
+                    if (result.status != 0) {
+                        reject(new Error('ldap search status is not 0, search failed'))
+                    } else {
+                        resolve(ldapObject)
+                    }
+                    ldapClient.unbind()
+                })
+            }
+        )
     })
-    ldapClient.search(
-      searchBase,
-      {
-        filter: filter,
-        scope: 'sub',
-      },
-      function (err, res) {
-        var user = null
-        if (err) {
-          reject(err)
-          ldapClient.unbind()
-          return
-        }
-        res.on('searchEntry', function (entry) {
-          user = entry.object
-        })
-        res.on('searchReference', function (referral) {
-          console.log('referral: ' + referral.uris.join())
-        })
-        res.on('error', function (err) {
-          reject(err)
-          ldapClient.unbind()
-        })
-        res.on('end', function (result) {
-          if (result.status != 0) {
-            reject(new Error('ldap search status is not 0, search failed'))
-          } else {
-            resolve(user)
-          }
-          ldapClient.unbind()
-        })
-      }
-    )
-  })
 }
 
 // search a groups which user is member
 async function _searchUserGroups(
-  ldapClient,
-  searchBase,
-  user,
-  groupClass,
-  groupMemberAttribute = 'member'
+    ldapClient,
+    searchBase,
+    user,
+    groupClass,
+    groupMemberAttribute = 'member'
 ) {
-  return new Promise(function (resolve, reject) {
-    ldapClient.search(
-      searchBase,
-      {
-        filter: `(&(objectclass=${groupClass})(${groupMemberAttribute}=${user.dn}))`,
-        scope: 'sub',
-      },
-      function (err, res) {
-        var groups = []
-        if (err) {
-          reject(err)
-          ldapClient.unbind()
-          return
-        }
-        res.on('searchEntry', function (entry) {
-          groups.push(entry.object)
-        })
-        res.on('searchReference', function (referral) {
-          console.log('referral: ' + referral.uris.join())
-        })
-        res.on('error', function (err) {
-          reject(err)
-          ldapClient.unbind()
-        })
-        res.on('end', function (result) {
-          if (result.status != 0) {
-            reject(new Error('ldap search status is not 0, search failed'))
-          } else {
-            resolve(groups)
-          }
-          ldapClient.unbind()
-        })
-      }
-    )
-  })
+    return new Promise(function (resolve, reject) {
+        ldapClient.search(
+            searchBase,
+            {
+                filter: `(&(objectclass=${groupClass})(${groupMemberAttribute}=${user.dn}))`,
+                scope: 'sub',
+            },
+            function (err, res) {
+                var groups = []
+                if (err) {
+                    reject(err)
+                    ldapClient.unbind()
+                    return
+                }
+                res.on('searchEntry', function (entry) {
+                    groups.push(entry.object)
+                })
+                res.on('searchReference', function (referral) {
+                    console.log('referral: ' + referral.uris.join())
+                })
+                res.on('error', function (err) {
+                    reject(err)
+                    ldapClient.unbind()
+                })
+                res.on('end', function (result) {
+                    if (result.status != 0) {
+                        reject(new Error('ldap search status is not 0, search failed'))
+                    } else {
+                        resolve(groups)
+                    }
+                    ldapClient.unbind()
+                })
+            }
+        )
+    })
 }
 
 async function authenticateWithAdmin(
-  adminDn,
-  adminPassword,
-  userSearchBase,
-  usernameAttribute,
-  username,
-  userPassword,
-  starttls,
-  ldapOpts,
-  groupsSearchBase,
-  groupClass,
-  groupMemberAttribute = 'member'
-) {
-  var ldapAdminClient
-  try {
-    ldapAdminClient = await _ldapBind(
-      adminDn,
-      adminPassword,
-      starttls,
-      ldapOpts
-    )
-  } catch (error) {
-    throw { admin: error }
-  }
-  var user = await _searchUser(
-    ldapAdminClient,
+    adminDn,
+    adminPassword,
     userSearchBase,
     usernameAttribute,
-    username
-  )
-  ldapAdminClient.unbind()
-  if (!user || !user.dn) {
-    ldapOpts.log &&
-      ldapOpts.log.trace(
-        `admin did not find user! (${usernameAttribute}=${username})`
-      )
-    throw new LdapAuthenticationError(
-      'user not found or usernameAttribute is wrong'
-    )
-  }
-  var userDn = user.dn
-  let ldapUserClient
-  try {
-    ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
-  } catch (error) {
-    throw error
-  }
-  ldapUserClient.unbind()
-  if (groupsSearchBase && groupClass && groupMemberAttribute) {
+    username,
+    userPassword,
+    starttls,
+    ldapOpts,
+    groupsSearchBase,
+    groupClass,
+    groupMemberAttribute = 'member'
+) {
+    var ldapAdminClient
     try {
-      ldapAdminClient = await _ldapBind(
-        adminDn,
-        adminPassword,
-        starttls,
-        ldapOpts
-      )
+        ldapAdminClient = await _ldapBind(
+            adminDn,
+            adminPassword,
+            starttls,
+            ldapOpts
+        )
     } catch (error) {
-      throw error
+        throw {admin: error}
     }
-    var groups = await _searchUserGroups(
-      ldapAdminClient,
-      groupsSearchBase,
-      user,
-      groupClass,
-      groupMemberAttribute
+    var ldapObject = await _searchUser(
+        ldapAdminClient,
+        userSearchBase,
+        usernameAttribute,
+        username
     )
-    user.groups = groups
     ldapAdminClient.unbind()
-  }
-  return user
+    if (!ldapObject.user || !ldapObject.user.dn) {
+        ldapOpts.log &&
+        ldapOpts.log.trace(
+            `admin did not find user! (${usernameAttribute}=${username})`
+        )
+        throw new LdapAuthenticationError(
+            'user not found or usernameAttribute is wrong'
+        )
+    }
+    var userDn = ldapObject.user.dn
+    let ldapUserClient
+    try {
+        ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
+    } catch (error) {
+        throw error
+    }
+    ldapUserClient.unbind()
+    if (groupsSearchBase && groupClass && groupMemberAttribute) {
+        try {
+            ldapAdminClient = await _ldapBind(
+                adminDn,
+                adminPassword,
+                starttls,
+                ldapOpts
+            )
+        } catch (error) {
+            throw error
+        }
+        var groups = await _searchUserGroups(
+            ldapAdminClient,
+            groupsSearchBase,
+            ldapObject.user,
+            groupClass,
+            groupMemberAttribute
+        )
+        ldapObject.user.groups = groups
+        ldapAdminClient.unbind()
+    }
+    return ldapObject
 }
 
 async function authenticateWithUser(
-  userDn,
-  userSearchBase,
-  usernameAttribute,
-  username,
-  userPassword,
-  starttls,
-  ldapOpts,
-  groupsSearchBase,
-  groupClass,
-  groupMemberAttribute = 'member'
-) {
-  let ldapUserClient
-  try {
-    ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
-  } catch (error) {
-    throw error
-  }
-  if (!usernameAttribute || !userSearchBase) {
-    // if usernameAttribute is not provided, no user detail is needed.
-    ldapUserClient.unbind()
-    return true
-  }
-  var user = await _searchUser(
-    ldapUserClient,
+    userDn,
     userSearchBase,
     usernameAttribute,
-    username
-  )
-  if (!user || !user.dn) {
-    ldapOpts.log &&
-      ldapOpts.log.trace(
-        `user logged in, but user details could not be found. (${usernameAttribute}=${username}). Probabaly wrong attribute or searchBase?`
-      )
-    throw new LdapAuthenticationError(
-      'user logged in, but user details could not be found. Probabaly usernameAttribute or userSearchBase is wrong?'
-    )
-  }
-  ldapUserClient.unbind()
-  if (groupsSearchBase && groupClass && groupMemberAttribute) {
+    username,
+    userPassword,
+    starttls,
+    ldapOpts,
+    groupsSearchBase,
+    groupClass,
+    groupMemberAttribute = 'member'
+) {
+    let ldapUserClient
     try {
-      ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
+        ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
     } catch (error) {
-      throw error
+        throw error
     }
-    var groups = await _searchUserGroups(
-      ldapUserClient,
-      groupsSearchBase,
-      user,
-      groupClass,
-      groupMemberAttribute
+    if (!usernameAttribute || !userSearchBase) {
+        // if usernameAttribute is not provided, no user detail is needed.
+        ldapUserClient.unbind()
+        return true
+    }
+    var ldapObject = await _searchUser(
+        ldapUserClient,
+        userSearchBase,
+        usernameAttribute,
+        username
     )
-    user.groups = groups
+    if (!ldapObject.user || !ldapObject.user.dn) {
+        ldapOpts.log &&
+        ldapOpts.log.trace(
+            `user logged in, but user details could not be found. (${usernameAttribute}=${username}). Probabaly wrong attribute or searchBase?`
+        )
+        throw new LdapAuthenticationError(
+            'user logged in, but user details could not be found. Probabaly usernameAttribute or userSearchBase is wrong?'
+        )
+    }
     ldapUserClient.unbind()
-  }
-  return user
+    if (groupsSearchBase && groupClass && groupMemberAttribute) {
+        try {
+            ldapUserClient = await _ldapBind(userDn, userPassword, starttls, ldapOpts)
+        } catch (error) {
+            throw error
+        }
+        var groups = await _searchUserGroups(
+            ldapUserClient,
+            groupsSearchBase,
+            ldapObject.user,
+            groupClass,
+            groupMemberAttribute
+        )
+        ldapObject.user.groups = groups
+        ldapUserClient.unbind()
+    }
+    return ldapObject
 }
 
 async function authenticate(options) {
-  if (!options.userDn) {
-    assert(options.adminDn, 'Admin mode adminDn must be provided')
-    assert(options.adminPassword, 'Admin mode adminPassword must be provided')
-    assert(options.userSearchBase, 'Admin mode userSearchBase must be provided')
+    if (!options.userDn) {
+        assert(options.adminDn, 'Admin mode adminDn must be provided')
+        assert(options.adminPassword, 'Admin mode adminPassword must be provided')
+        assert(options.userSearchBase, 'Admin mode userSearchBase must be provided')
+        assert(
+            options.usernameAttribute,
+            'Admin mode usernameAttribute must be provided'
+        )
+        assert(options.username, 'Admin mode username must be provided')
+    } else {
+        assert(options.userDn, 'User mode userDn must be provided')
+    }
+    assert(options.userPassword, 'userPassword must be provided')
     assert(
-      options.usernameAttribute,
-      'Admin mode usernameAttribute must be provided'
+        options.ldapOpts && options.ldapOpts.url,
+        'ldapOpts.url must be provided'
     )
-    assert(options.username, 'Admin mode username must be provided')
-  } else {
-    assert(options.userDn, 'User mode userDn must be provided')
-  }
-  assert(options.userPassword, 'userPassword must be provided')
-  assert(
-    options.ldapOpts && options.ldapOpts.url,
-    'ldapOpts.url must be provided'
-  )
-  if (options.adminDn) {
-    assert(
-      options.adminPassword,
-      'adminDn and adminPassword must be both provided.'
+    if (options.adminDn) {
+        assert(
+            options.adminPassword,
+            'adminDn and adminPassword must be both provided.'
+        )
+        return await authenticateWithAdmin(
+            options.adminDn,
+            options.adminPassword,
+            options.userSearchBase,
+            options.usernameAttribute,
+            options.username,
+            options.userPassword,
+            options.starttls,
+            options.ldapOpts,
+            options.groupsSearchBase,
+            options.groupClass,
+            options.groupMemberAttribute
+        )
+    }
+    assert(options.userDn, 'adminDn/adminPassword OR userDn must be provided')
+    return await authenticateWithUser(
+        options.userDn,
+        options.userSearchBase,
+        options.usernameAttribute,
+        options.username,
+        options.userPassword,
+        options.starttls,
+        options.ldapOpts,
+        options.groupsSearchBase,
+        options.groupClass,
+        options.groupMemberAttribute
     )
-    return await authenticateWithAdmin(
-      options.adminDn,
-      options.adminPassword,
-      options.userSearchBase,
-      options.usernameAttribute,
-      options.username,
-      options.userPassword,
-      options.starttls,
-      options.ldapOpts,
-      options.groupsSearchBase,
-      options.groupClass,
-      options.groupMemberAttribute
-    )
-  }
-  assert(options.userDn, 'adminDn/adminPassword OR userDn must be provided')
-  return await authenticateWithUser(
-    options.userDn,
-    options.userSearchBase,
-    options.usernameAttribute,
-    options.username,
-    options.userPassword,
-    options.starttls,
-    options.ldapOpts,
-    options.groupsSearchBase,
-    options.groupClass,
-    options.groupMemberAttribute
-  )
 }
 
 class LdapAuthenticationError extends Error {
-  constructor(message) {
-    super(message)
-    // Ensure the name of this error is the same as the class name
-    this.name = this.constructor.name
-    // This clips the constructor invocation from the stack trace.
-    // It's not absolutely essential, but it does make the stack trace a little nicer.
-    //  @see Node.js reference (bottom)
-    Error.captureStackTrace(this, this.constructor)
-  }
+    constructor(message) {
+        super(message)
+        // Ensure the name of this error is the same as the class name
+        this.name = this.constructor.name
+        // This clips the constructor invocation from the stack trace.
+        // It's not absolutely essential, but it does make the stack trace a little nicer.
+        //  @see Node.js reference (bottom)
+        Error.captureStackTrace(this, this.constructor)
+    }
 }
 
 module.exports.authenticate = authenticate
