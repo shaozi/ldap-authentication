9a6b28db0fc0aea2100228c2d8f23fd51dd69eed
shaozi <jingshaochen@gmail.com>
2025-01-30 12:28:00 -0600

    escape , in dn (#76)
    * escape , in dn
    
    * typo
    
    * add _ldapEscapeDN to export for testing

--- test/string.spec.js
+++ test/string.spec.js
@@ -1,5 +1,5 @@
 const { exportForTesting } = require('../index.js')
-const { _isHex, _parseEscapedHexToUtf8, _recursiveParseHexString } =
+const { _isHex, _parseEscapedHexToUtf8, _recursiveParseHexString, _ldapEscapeDN } =
   exportForTesting
 
 describe('string conversion test', () => {
@@ -40,4 +40,23 @@ describe('string conversion test', () => {
     let converted = _recursiveParseHexString(obj)
     expect(converted).toEqual(target)
   })
+})
+
+describe('escape , in the DN test', ()=>{
+  let cases = [
+    { s: "a", want: "a" },
+    { s: "", want: "" },
+    { s: "CN=a,DN=b", want: "CN=a,DN=b" },
+    { s: "CN=a, c,DN=b", want: "CN=a\\, c,DN=b" },
+    { s: "CN=a\\, c,DN=b", want: "CN=a\\, c,DN=b" },
+    { s: "CN=a, b, c,DN=b", want: "CN=a\\, b\\, c,DN=b" },
+    { s: "CN=a, c", want: "CN=a\\, c" },
+  ]
+  for (let c of cases) {
+    it("escape "+c.s, ()=>{
+      let got = _ldapEscapeDN(c.s)
+      expect(got).toEqual(c.want)
+    })
+    
+  }
 })
