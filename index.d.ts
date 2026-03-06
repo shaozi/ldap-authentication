5385a4b27cf95b938e11d0366c38c36893593399
Daniel Haupt <mail@danielhaupt.de>
2026-03-06 18:29:55 +0100

    Add missing types for AuthenticationResult (#97)
    Fixes #96

--- index.d.ts
+++ index.d.ts
@@ -20,7 +20,24 @@ declare module 'ldap-authentication' {
     explicitBufferAttributes?: string[]
   }
 
+  export const AUTH_RESULT_FAILURE = 0
+  export const AUTH_RESULT_SUCCESS = 1
+  export const AUTH_RESULT_FAILURE_IDENTITY_NOT_FOUND = -1
+  export const AUTH_RESULT_FAILURE_IDENTITY_AMBIGUOUS = -2
+  export const AUTH_RESULT_FAILURE_CREDENTIAL_INVALID = -3
+  export const AUTH_RESULT_FAILURE_UNCATEGORIZED = -4
+
+  export class AuthenticationResult {
+    constructor(authCode: number, identity: string, user: any, messages: string[], client: any)
+    readonly code: number
+    readonly identity: string
+    readonly user: any
+    readonly messages: string[]
+    readonly client: any
+  }
+
   export function authenticate(options: AuthenticationOptions): Promise<any>
+  export function authenticateResult(options: AuthenticationOptions): Promise<AuthenticationResult>
 
   export class LdapAuthenticationError extends Error {
     constructor(message: any)
